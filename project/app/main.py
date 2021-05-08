1673921057af24bb44215d94e1a44e1ac6bf312e
Youngjae <limyoungjae@gmail.com>
2021-05-08 19:57:33 -0400

    Added the remaining restful routes and tests

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import FastAPI, Depends
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -11,6 +11,9 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
 
     return application
 
